1183e513399f6d62586df0af59341bb872d20c4d
Douglas Kiang <dkiang@cs50.harvard.edu>
2019-03-03 04:24:58 +0000

    MoveRight completed

--- 2048.c
+++ 2048.c
@@ -104,4 +104,24 @@ void moveLeft(int board[])
 
 void moveRight(int board[])
 {
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i] == board[i + 1])
+        {
+            board[i] *= 2;
+            board[i + 1] = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = i + 1; j < 4; j++)
+        {
+            if (board[j] == 0 && board[i] > 0)
+            {
+                board[j] = board[i];
+                board[i] = 0;
+            }
+        }
+    }
 }
